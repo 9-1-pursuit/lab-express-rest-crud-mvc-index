c94e86ee8269d6fd81ee118f0ebc8da0f57c530e
bowersjuan <bowersjuan@gmail.com>
2022-12-15 13:54:46 -0500

    Add and update controllers

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const equipment = require("./controllers/machines.controller");
+const locations = require("./controllers/locations.controller");
+const people = require("./controllers/persons.controller");
+const plans = require("./controllers/plans.controller");
+const specialEvents = require("./controllers/special-events.controller");
 const app = express();
+
+app.use("/equipment", equipment);
+
+app.use("/locations", locations);
+
+app.use("/people", people);
+
+app.use("/plans", plans);
+
+app.use("/special-events", specialEvents);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
