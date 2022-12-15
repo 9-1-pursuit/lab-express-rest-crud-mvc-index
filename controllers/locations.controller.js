c0421a1b214967966735aa699c0895000b33e6cc
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-15 18:08:14 -0500

    bonus complete, just not sure if data for just locations route should remain or display updated info as well

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,10 +1,30 @@
 const express = require("express")
 const locations = express.Router()
 const data = require("../model/location.model.js")
+const bonusData = [...require("../__tests__/sample-data").locations]
+const personData = require("../model/person.model.js")
 
 locations.get("/", (req, resp) => {
     resp.send(data)
 })
 
+// BONUS
+locations.get("/people", (req, resp) => {
+    const [people, members ] = personData.reduce((acc, person) => {
+        const index = person.mainLocation === "11101" ? 0 : 1
+        acc[index].push(person)
+        return acc
+    }, [[], []])
+    bonusData.forEach(obj => {
+        if(obj.zip === "11101"){
+            obj.people = people
+        }
+        if(obj.zip ==="11011"){
+            obj.members = members
+        }
+    })
+
+    resp.send(bonusData)
+})
 
 module.exports = locations
