8851dcf49f822192d04e902bebc0a06166907e28
Lennie Nurse <lnursej@gmail.com>
2022-12-16 11:16:11 -0500

    "update controllers change C to c "

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require("express")
 
 const app = express()
 
-const locationsController = require("./controllers/locations.Controller")
+const locationsController = require("./controllers/locations.controller")
 app.use("/locations", locationsController)
 
-const machinesController = require("./controllers/machines.Controller")
+const machinesController = require("./controllers/machines.controller")
 app.use("/machines", machinesController)
 
-const personsController = require("./controllers/persons.Controller")
+const personsController = require("./controllers/persons.controller")
 app.use("/persons", personsController)
 
-const plansController = require("./controllers/plans.Controller")
+const plansController = require("./controllers/plans.controller")
 app.use("/plans", plansController)
 
-const specialEventsController = require("./controllers/special-events.Controller")
+const specialEventsController = require("./controllers/special-events.controller")
 app.use("/special-events", specialEventsController)
 
 app.get("/", (req, res) => {
