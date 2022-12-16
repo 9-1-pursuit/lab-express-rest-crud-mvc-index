473c501b522418e1470b57efeb66f5373bde9aaa
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-12-15 20:45:59 -0500

    tests passing

--- controllers/special-events.controller.js
+++ controllers/special-events.controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const specialEvents = express.Router();
-const specialEventsArr = require("../models/special-events.model");
+const specialEventsArr = require("../models/special-event.model");
 
 specialEvents.get("/", (req, res) => {
   res.status(200).json(specialEventsArr);
