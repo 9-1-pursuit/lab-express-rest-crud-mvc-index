dd3c90e0984efb3206cab97a408c9c237b1e8dfd
melanieinpursuit <melaniegonzalez@pursuit.org>
2023-01-12 06:07:39 -0500

    Passing all tests

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ const plansController = require('./controllers/plans.controller')
 const specialEventsController = require('./controllers/special-events.controller')
 
 app.use('/locations', locationsController)
-app.use('/equipment', machinesController)
 app.use('/machines', machinesController)
 app.use('/persons', peopleController)
-app.use('/people', peopleController)
 app.use('/plans', plansController)
 app.use('/special-events', specialEventsController)
 
